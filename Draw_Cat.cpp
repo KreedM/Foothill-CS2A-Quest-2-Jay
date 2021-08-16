8bc25847f893a2a2bf1a7efa7eedf2ab4509d98e
KreedM <blackknight3377@gmail.com>
2021-08-16 11:43:59 -0700

    Added Files.

---
+++ Draw_Cat.cpp
@@ -0,0 +1,25 @@
+// Derek Meng
+
+// Draw_Cat.cpp
+
+#include <iostream>
+
+using namespace std;
+
+void draw_cat() {
+  cout << "  -------\n"
+     << " | /\\_/\\ |\n"
+     << " |( o o )|\n"
+     << " | > ^ < |\n"
+     << "  -------\n"
+     << "Schrodinger\n" ;
+}
+
+int main(int argc, const char * argv[]) {
+// TODO - Invoke your function from here.
+// I'll invoke your main()
+
+  draw_cat();
+
+  return 0;
+}
---
+++ Limerick.cpp
@@ -0,0 +1,34 @@
+// Derek Meng
+
+// Limerick.cpp
+
+// 2a-Lab-01
+
+#include <iostream>
+#include <sstream> 
+#include <cmath> 
+#include <cstdlib> 
+
+using namespace std;
+
+double eval_limerick(int dozen, int gross, int score) {
+  return ( dozen + gross + score + 3 * sqrt(4) ) / 7 + 5 * 11;
+}
+
+
+int main(int argc, char **argv) {
+  int dozen, gross, score;
+  
+  if (argc < 4) {
+    cerr <<"Usage: limerick dozen-val gross-val score-val\n";
+    exit(1);
+  }
+  
+  istringstream(argv[1]) >>dozen;
+  istringstream(argv[2]) >>gross;
+  istringstream(argv[3]) >>score; 
+  
+  cout << eval_limerick(dozen, gross, score) << endl;
+
+  return 0;
+}
